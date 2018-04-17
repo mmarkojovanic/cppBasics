e9628d770d247d029982df5ddb3cddc912fb6550
mmarkojovanic <mmarkojovanic@gmail.com>
2018-04-17 15:55:42 +0200

    Update main.cpp
    v2

--- main.cpp
+++ main.cpp
@@ -5,19 +5,7 @@
 using namespace std;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-int total_sum = 0;
 bool lock = false;
-int*A;
-int*B;
-int*C;
-
-struct arg_struct {
-	int threadid;
-    int* arg1;
-    int* arg2;
-    int* arg3;
-};
 
 void *PrintHello(void *threadid) {
    	long tid = (long)threadid;
@@ -28,28 +16,6 @@ void *PrintHello(void *threadid) {
 //   	pthread_mutex_unlock(&mutex);
    	pthread_exit(NULL);
 }
-//void *SUM(void *args)
-//{
-//	arg_struct args1 = (arg_struct)(*args);
-//	int tid = args1.threadid;
-//	int* A = args1.arg1;
-//	int* B = args1.arg2;
-//	int* C = args1.arg3;
-////   	long tid = (long)threadid;
-//	int sum = 0;
-//	for(int i = 0; i<100; i++)
-//	{
-//		while(lock); lock = true;
-//   		cout << "Hello World! Thread ID, " << tid << " iteration: " << i << endl;
-//   		lock = false;
-//		C[tid*100+i]=A[tid*100+i]+B[tid*100+i];
-//		sum+=C[tid*100+i];
-//	}
-//	while(pthread_mutex_lock(&mutex));
-//	total_sum += sum;
-//	pthread_mutex_unlock(&mutex);
-//	pthread_exit(NULL);
-//}
 
 int main () {
 	pthread_t threads[NUM_THREADS];
@@ -74,32 +40,7 @@ int main () {
 //   	pthread_mutex_unlock(&mutex);
    	for( i = 0; i < NUM_THREADS; i++ )
    		pthread_join(threads[i], NULL);
-   	
-//   	for(int i=0; i<1000; i++)
-//   	{
-//		A[i]= i;
-//		B[i]= A[i]+i;
-//	}
-//	// now we want to A+B; and find total sum
-//	A = new int[100*NUM_THREADS];
-//	B = new int[100*NUM_THREADS];
-//	C = new int[100*NUM_THREADS];
-//	for(i=0; i<NUM_THREADS; i++)
-//	{
-//		arg_struct* args; args->arg1 = A; args->arg2 = B; args->arg3 = C; args->threadid = i;
-//		void* params = (void*)(args);
-//   		rc = pthread_create(&threads[i], NULL, SUM, params);
-//   		if (rc) {
-//        	 cout << "Error:unable to create thread," << rc << endl;
-//        	 exit(-1);
-//      	}
-//   	}
-//	for(i = 0; i < NUM_THREADS; i++)
-//   		pthread_join(threads[i], NULL);
-//   		
-//   	cout << "Total sum: " << total_sum << endl;
-
-   	
+   	 	
    	
    	pthread_exit(NULL);
 }
